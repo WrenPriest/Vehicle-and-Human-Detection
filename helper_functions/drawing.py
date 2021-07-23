ac10ada0fad5eedf08a6a2d7c68fc9ab245bd312
Wren <85448134+WrenPriest@users.noreply.github.com>
2021-07-23 17:40:52 -0500

    cleaning up imports and PEP8

--- helper_functions/drawing.py
+++ helper_functions/drawing.py
@@ -1,28 +1,29 @@
-import cv2
-
-def draw_roi(current_frame,width,height,is_vehicle_detected,ROI_Line):
+def draw_roi(current_frame, width, height, is_vehicle_detected, ROI_Line):
     if ROI_Line == 'vertical':
-        roi_start = (int(width/2), 0)
-        rot_end = (int(width/2), height)
+        roi_start = (int(width / 2), 0)
+        rot_end = (int(width / 2), height)
     else:
-        roi_start = (0, int(height/2))
-        rot_end = (width, int(height/2))
+        roi_start = (0, int(height / 2))
+        rot_end = (width, int(height / 2))
 
     if is_vehicle_detected is True:
         cv2.line(current_frame, roi_start, roi_end, (0, 0, 0xFF), 5)
     else:
         cv2.line(current_frame, roi_start, roi_end, (0, 0xFF, 0), 5)
 
-def draw_detection_boxes(boxes,current_frame):
+
+def draw_detection_boxes(boxes, current_frame):
     H, W, _ = current_frame.shape
     for x in range(boxes):
         y1, x1, y2, x2 = boxes[x]
-        x1 *= W 
-        x2 *= W 
+        x1 *= W
+        x2 *= W
         y1 *= H
         y2 *= H
         cv2.rectangle(current_frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
-def draw_counter(current_frame,total_vehicles_detected):
+
+
+def draw_counter(current_frame, total_vehicles_detected):
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(
         current_frame,
@@ -33,4 +34,4 @@ def draw_counter(current_frame,total_vehicles_detected):
         (0, 0xFF, 0xFF),
         2,
         cv2.FONT_HERSHEY_SIMPLEX,
-        )
+    )
