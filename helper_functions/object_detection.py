73c76b861a13c817c194028ebf5263921707e929
Wren <85448134+WrenPriest@users.noreply.github.com>
2021-07-23 15:40:20 -0500

    finished object_detection.py

--- helper_functions/object_detection.py
+++ helper_functions/object_detection.py
@@ -1,3 +1,32 @@
 def object_detection(current_frame):
-    (im_width, im_height) = current_frame.size
-    frame_array = np.array(current_frame)
+    # convert to tensor for model
+    frame_tensor = tf.convert_to_tensor([current_frame], dtype=tf.uint8)
+    # run frame_tensor through model
+    detections = model(frame_tensor)
+
+    # extract information from model detections
+    num_detect = int(detections[0].numpy()[0])
+    classes = detections[1].numpy()[0, :num_detect]
+    # scores = detections[2].numpy()[0, :num_detect]
+    boxes = detections[3].numpy()[0, :num_detect]
+
+    box_centers = []
+
+    if ROI_line == 'vertical':
+        for x in range(num_detect):
+            box = boxes[x]
+            y1, x1, y2, x2 = box
+            y1 *= height
+            y2 *= height
+            middle = int((y1 + y2) / 2)
+            box_centers.append(middle)
+    else:
+        for x in range(num_detect):
+            box = boxes[x]
+            y1, x1, y2, x2 = box
+            x1 *= width
+            x2 *= width
+            middle = int((y1 + y2) / 2)
+            box_centers.append(middle)
+
+    return num_detect, classes, boxes, box_centers
