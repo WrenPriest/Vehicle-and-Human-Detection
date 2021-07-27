5bf9bfcd21fea284649df467b75255d40539553a
jmarti44 <jmarti1997.jm@gmail.com>
2021-07-26 20:48:28 -0500

    command line arguments

--- helper_functions/counting.py
+++ helper_functions/counting.py
@@ -1,18 +1,18 @@
-def counting(box_centers, classes, width, counter):
+def counting(box_centers, classes, width, counter,vehicle_sensitivity,pedestrian_sensitivity):
     is_vehicle_detected = False
-    print(classes)
+    #print(classes)
     for i in range(len(box_centers)):
         #print(classes[i])
         if classes[i] in [3,6,8]:
-            if box_centers[i] in range(int(width / 2 - .02 * width),
-                                       int(width / 2 + .02 * width)):
+            if box_centers[i] in range(int(width / 2 - vehicle_sensitivity * width),
+                                       int(width / 2 + vehicle_sensitivity * width)):
                 counter += 1
                 is_vehicle_detected = True
             else:
                 is_vehicle_detected = False
         elif classes[i] == 1:
-            if box_centers[i] in range(int(width / 2 - .003 * width),
-                                       int(width / 2 + .005 * width)):
+            if box_centers[i] in range(int(width / 2 - pedestrian_sensitivity * width),
+                                       int(width / 2 + pedestrian_sensitivity * width)):
                 counter += 1
                 is_vehicle_detected = True
             else:
