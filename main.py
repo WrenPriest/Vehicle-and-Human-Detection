1e1073ecf647c11fddcda3cf7d016afc901318fc
Wren <85448134+WrenPriest@users.noreply.github.com>
2021-07-27 22:38:48 -0500

    Added: Debugging window Separate counters for vehicles and pedestrians Error Handling Only draw boxes if classes in [1, 3, 6, 8]

--- main.py
+++ main.py
@@ -5,15 +5,7 @@ from helper_functions.object_detection import object_detection
 from helper_functions.counting import counting
 from helper_functions import drawing
 
-# Command line parameters:
-# ROI_orientation = horizontal/vertical
-# vehicle_sensitivity = 0.02
-# pedestrian_sensitivity = 0.001
-# Change ROI_line variable to ROI_orientation
-
-
 # Getting key global detection variables
-
 print("Specify if the program should use default values for the detection variables (y,n): ")
 choice = input()
 if choice == 'n':
@@ -53,12 +45,13 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 ticker = 0
-counter = 0
+vehicle_counter = 0
+pedestrian_counter = 0
 
 # output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-# output = cv2.VideoWriter(source_video.split(".")[0] + '_output.mp4', fourcc, fps, (width, height))
-output = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
+output = cv2.VideoWriter(source_video.split(".")[0] + '_output.mp4', fourcc, fps, (width, height))
+# output = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
 
 # Command line parameters:
 # ROI_orientation = horizontal/vertical
@@ -74,20 +67,23 @@ while cap.isOpened():
         # progress ticker
         print(f"Processing frame {ticker} of {num_frames}")
         # object detection helper function
-        (num_detect, classes, boxes, box_centers) = object_detection(current_frame, model, ROI_ORIENTATION, height,
-                                                                     width)
+        (num_detect, classes, boxes, box_centers) = \
+            object_detection(current_frame, model, ROI_ORIENTATION, height, width)
 
         # counting helper function
         if ticker % 2 == 0:
-            (counter, is_vehicle_detected) = counting(box_centers, classes, width, counter, vehicle_sensitivity,
-                                                      pedestrian_sensitivity)
+            (vehicle_counter, pedestrian_counter, is_vehicle_detected) = \
+                counting(box_centers, classes, width, vehicle_counter, pedestrian_counter,
+                         vehicle_sensitivity, pedestrian_sensitivity)
 
         # drawing helper functions
         drawing.draw_roi(current_frame, width, height, is_vehicle_detected, ROI_ORIENTATION)
         drawing.draw_detection_boxes(current_frame, num_detect, boxes, labels, classes)
-        drawing.draw_counter(current_frame, counter)
+        drawing.draw_counter(current_frame, vehicle_counter, pedestrian_counter)
 
         output.write(current_frame)
+        # uncomment for debugging
+        # cv2.imshow('debugging', current_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
